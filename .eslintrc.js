2a26d1998286c066f21ca63e7022c1f045db1590
schremsn <josef@dieselpark.com>
2017-12-19 09:07:23 +0700

    lint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,35 @@
 module.exports = {
-  extends: 'airbnb',
-  plugins: ['react', 'jsx-a11y', 'import'],
-  rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    "no-use-before-define": 0,
-  },
-};
+
+    "parser": "babel-eslint",
+
+    "extends": "airbnb",
+
+    "plugins": ["react", "jsx-a11y", "import"],
+
+    "rules": {
+
+        "react/jsx-filename-extension": ["off"],
+
+        "linebreak-style": ["off"],
+
+        "no-undef": ["error"],
+
+        "react/sort-comp": ["off"],
+
+        "react/prefer-stateless-function": ["off"]
+
+    },
+
+    "globals": {
+
+        "it": 0,
+
+        "expect": 0,
+
+        "describe": 0,
+
+        "navigator": 0
+
+    }
+
+}
